0df9a5a833c78bdf8291399a91faf997ecf1d20b
matt-violet <mattviolet@gmail.com>
2019-04-12 15:47:13 -0700

    tree.js passes

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,37 +3,65 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // [] contining child trees
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // sets value as target of a node
-  // adds node as child of the tree
+  var newInstance = Tree(value); // newInstance = a tree with .value = value
+  this.children.push(newInstance); // adds instance as a child of the tree from which it was created
 };
 
 treeMethods.contains = function(target) {
+  var currentNode = this;
+  if (currentNode.value === target) { //- base case
+    return true;
+  }
+  if (currentNode.children.length) { // else if currentNode.children.length > 0 
+    for (var i = 0; i < this.children.length; i++) { // searches entire tree data structure
+      var childNode = this.children[i];
+      if (childNode.contains(target)) { // return call recursively on each child
+        return true;
+      }
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
 /*
+// target === 'Daniel';
 
- newTree = {
-   value = value,
-   children = [{}, {}, {}],
+tree5.contains(target);
+
+ tree5 = {
+   value: value,
+   children: [{danielTree}], // === 1
+   addChild: function, // this = tree
+   contains: function
+ }
+
+tree5.addChild('Daniel');
+
+tree5[i].contains(target);
+
+ danielTree = {
+   value = 'Daniel',
+   children = [],
+   addChild: function,
+   contains: function
  }
 
  treeMethods = {
    addChild = {f},
    contains = {f}
  }
+
+*/
