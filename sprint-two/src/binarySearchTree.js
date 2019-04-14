3213dd1c0f7563220a2381cd7e106ff14aaa3c5d
matt-violet <mattviolet@gmail.com>
2019-04-13 18:15:19 -0700

    Implement pseudocode for binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function(value) { // prototypal instantiation pattern => Object.create(Tree, proto)
+  // var tree = Object.create(BinarySearchTree.prototype);
+
+  this.value = value;
+  this.leftChild = null;
+  this.rightChild = null;
+
+  // return tree;
 };
 
+BinarySearchTree.prototype.left = function(value) {
+  // traverse binary tree to the left (towards a smaller value)
+  // call function on this.leftChild
+};
+
+BinarySearchTree.prototype.right = function(value) {
+  // traverse tree to the right (towards a larger value)
+  // call function on this.rightChild
+};
+
+BinarySearchTree.prototype.insert = function(value) { 
+  // add a new tree at the proper location
+  // utilizes .left and .right to find the proper location
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  // conducts a binary search using .left and .right
+  // 
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+efficient way to search recursively
+left side is less than & right side is greater than
+each tree has at most 2 children **
+binary search trees are always sorted
+
+ensure we only recurse down a single path (branch)
+
+var tree = Object.create(BinarySearchTree.prototype);
+
+tree = {
+  left: {f},
+  right: {f},
+  insert: {f},
+  contains: {f},
+  depthFirstLog: {f}
+}
+
+
+*/
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -44,12 +44,9 @@ HashTable.prototype.remove = function(k) {
   }
 };
 
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
 
 /*
 var hash = new HashTable;
